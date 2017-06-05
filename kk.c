e95c1b3538e39cda773c1b9f1d90c7df0353079b
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-06-05 15:59:23 +0530

    Add files via upload

---
+++ kk.c
@@ -0,0 +1,11 @@
+#include<stdioh>
+int main()
+{
+  char a;
+  printf("enter the character\n");
+  scanf("%d",&a);
+  if(ch>="a"&&ch<="z")(ch>="A"&&ch<="Z");
+  printf("the character a is alphabet");
+  else
+  printf("the character a is not alphabet");
+  }
